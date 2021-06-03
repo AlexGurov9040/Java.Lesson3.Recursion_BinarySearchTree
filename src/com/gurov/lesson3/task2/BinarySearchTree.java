fd863e4d71e2ce8fe7b2ebc278254e815d31870d
AlexGurov9040 <www.pers93@mail.ru>
2021-06-03 21:47:25 +0300

    Third commit: change BinarySearchTree and CreateTree methods

--- src/com/gurov/lesson3/task2/BinarySearchTree.java
+++ src/com/gurov/lesson3/task2/BinarySearchTree.java
@@ -13,8 +13,22 @@ public class BinarySearchTree {
         CreateTree tree = new CreateTree();
         tree.Insert(32,"Node_1");
         tree.Insert(16,"Node_2");
-        tree.Insert(34,"Node_3");
-        tree.Display(tree);
+        tree.Insert(44,"Node_3");
+        tree.Insert(21,"Node_4");
+        tree.Insert(21,"Node_5");
+        tree.Insert(36,"Node_6");
+        tree.Insert(19,"Node_7");
+        tree.Display();
+        CreateNode findValue = tree.Find(21);
+        if (findValue != null)
+            System.out.printf("Node found: %d:%s\n",findValue.getKey(),findValue.getValue());
+        else
+            System.out.println("Node not found");
+        findValue = tree.Find(20);
+        if (findValue != null)
+            System.out.printf("Node found: %d:%s\n",findValue.getKey(),findValue.getValue());
+        else
+            System.out.println("Node not found");
     }
 
 }
--- src/com/gurov/lesson3/task2/CreateTree.java
+++ src/com/gurov/lesson3/task2/CreateTree.java
@@ -1,12 +1,11 @@
 package com.gurov.lesson3.task2;
 
-import java.util.ArrayDeque;
 import java.util.Stack;
 
 public class CreateTree {
 
     private CreateNode rootNode;
-    private static int gap = 32;
+    private CreateNode newNode;
 
     public CreateTree(){
 
@@ -15,30 +14,37 @@ public class CreateTree {
 
     public CreateNode Insert(int key,String value){
 
-        CreateNode newNode = new CreateNode(key,value);
         if (rootNode == null){ //if this root
-            rootNode = newNode;
-            return newNode;
+            rootNode = new CreateNode(key,value);
+            newNode = rootNode;
+            return rootNode;
         }
         else{ //if this not root
+                CreateNode node = newNode;
             if (key < newNode.getKey()){ //go to left
                 newNode = newNode.getLeftChild();
+                if (newNode == null) { //write new node
+                    node.setLeftChild(new CreateNode(key,value));
+                    newNode = rootNode;
+                    return rootNode;
+                }
+                else //search node next
+                    return Insert(key, value);
             }
             else if (key > newNode.getKey()){ //go to right
                 newNode = newNode.getRightChild();
+                if (newNode == null) { //write new node
+                    node.setRightChild(new CreateNode(key,value));
+                    newNode = rootNode;
+                    return rootNode;
+                }
+                else //search node next
+                    return Insert(key, value);
             }
             else { //if keys are equal then rewrite
-                newNode.setKey(key);
-                newNode.setValue(value);
-                return newNode;
+                node.setValue(value);
+                return rootNode;
             }
-            if (newNode == null) { //write new node
-                newNode.setKey(key);
-                newNode.setValue(value);
-                return newNode;
-            }
-            else //search node next
-                return Insert(key, value);
         }
     }
 
@@ -46,24 +52,59 @@ public class CreateTree {
 
     }
 
-    public int Find(int key){
+    public CreateNode Find(int key){
 
-        return key;
+        CreateNode node = rootNode;
+        while (node.getKey() != key){
+            if (key < node.getKey())
+                node = node.getLeftChild();
+            else
+                node = node.getRightChild();
+            if (node == null)
+                return null;
+        }
+        return node;
     }
 
-    public CreateTree Display(CreateTree tree){
+    public void Display(){
 
         if (rootNode == null){ //if tree is empty
             System.out.println("The tree is empty");
-            return tree;
         }
-        else{
-            for (int i=0;i<gap;i++)
-                System.out.print(" ");
-            Stack masNodes = new Stack();
-            masNodes.push(rootNode);
-
+        else{ //tree is not empty
+            Stack topLevel = new Stack();
+            Stack lowerLevel = new Stack();
+            String separator = "#########################################################################################";
+            System.out.println(separator);
+            topLevel.push(rootNode);
+            int gap = 40;
+            boolean notEmptyRow = true;
+            while (notEmptyRow){
+                notEmptyRow = false;
+                for (int i=0;i<gap;i++)
+                    System.out.print(" ");
+                while (!topLevel.empty()){
+                    CreateNode node = (CreateNode) topLevel.pop();
+                    if (node != null){
+                        lowerLevel.push(node.getLeftChild());
+                        lowerLevel.push(node.getRightChild());
+                        if (node.getLeftChild() != null || node.getRightChild() != null)
+                            notEmptyRow = true;
+                        System.out.print(node.getKey()+":"+node.getValue());
+                    }
+                    else
+                        System.out.print("-----");
+                    for (int i=0;i<gap;i++)
+                        System.out.print(" ");
+                }
+                while (!lowerLevel.empty()){
+                    topLevel.push(lowerLevel.pop());
+                }
+                System.out.println();
+                gap /= 2;
+            }
+            System.out.println(separator);
         }
-        return tree;
     }
+
 }
